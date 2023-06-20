71b429294a8b81bc661aad36fc94b5482b84ce9d
Danial Firdaus <danfird7@gmail.com>
2023-06-20 13:44:15 +0100

    index-show

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,4 +2,11 @@ class Venue < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :reviews
+
+  def check_availability(date)
+    conflicting_bookings = bookings.where('start_date <= ? AND end_date >= ?', date, date)
+    return true if conflicting_bookings.empty?
+
+    false
+  end
 end
