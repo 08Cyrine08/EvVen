9cd71a8d07bbc73ecbadfee2e8b5f8b68e420722
cyrine ettriki <cyrineettriki@hotmail.fr>
2023-06-30 17:14:12 +0200

    added method to now allow booking in past

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,14 @@ class Booking < ApplicationRecord
   validates :booking_end_date, presence: true
   belongs_to :user
   belongs_to :venue
+
+  def booking_dates_cannot_be_in_the_past
+    if booking_start_date && booking_start_date < Date.today
+      errors.add(:booking_start_date, "cannot be in the past")
+    end
+
+    if booking_end_date && booking_end_date < Date.today
+      errors.add(:booking_end_date, "cannot be in the past")
+    end
+  end
 end
