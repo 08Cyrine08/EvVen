27fc6b5f1ddd670b8a2ee00f0a688e1ffea0b0d2
cyrine ettriki <cyrineettriki@hotmail.fr>
2023-07-05 21:47:37 +0200

    geo

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -2,11 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="map"
 export default class extends Controller {
-  static values = { apiKey: String }
+  static values = {
+    apiKey: String,
+    markers: Array
+   }
+
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
-    container: this.element,
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
     });
+    this.#addMarkersToMap()
+  }
+
+  #addMarkersToMap() {
+    this,markersValue.forEach((marker) => {
+      popup.setLngLat([-122.4194, 37.7749]);
+    })
   }
 }
