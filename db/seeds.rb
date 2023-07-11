f3efed7832a53a36a0c5668c3dc49bc75624bdd6
cyrine ettriki <cyrineettriki@hotmail.fr>
2023-07-11 22:02:18 +0200

    layoutmap

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,69 +25,6 @@ user3.save
 
 puts "Creating venues"
 
-wedding1 = URI.open("https://res.cloudinary.com/dulcgeiwe/image/upload/v1687977109/Wedding-house-1_j8te7v.jpg")
-wedding2 = URI.open("https://res.cloudinary.com/dulcgeiwe/image/upload/v1687977109/Wedding-house-2_hrionv.jpg")
-wedding = Venue.new(
-  name: "Rose Garden",
-  location: "Paris, France",
-  description: "Are you looking for the perfect place to tie the knot? Look no further than the Rose Garden, a stunning
-  wedding venue that will make your special day unforgettable. The Rose Garden offers a beautiful outdoor setting with
-  lush greenery, fragrant roses, and a romantic gazebo. Whether you want a small intimate ceremony or a lavish reception,
-  we can accommodate your needs and budget. You and your guests will enjoy our delicious catering, professional service,
-  and elegant decor. Contact us today to book a tour and see why the Rose Garden is the best choice for your wedding.
-  ",
-  price: 1000
-)
-wedding.photos.attach(io: wedding1, filename: "wedding-1", content_type: "image/png")
-wedding.photos.attach(io: wedding2, filename: "wedding-2", content_type: "image/png")
-wedding.user = user1
-wedding.save
-
-open1 = URI.open("https://res.cloudinary.com/dulcgeiwe/image/upload/v1687977109/open-space-1_y91r8n.jpg")
-open2 = URI.open("https://res.cloudinary.com/dulcgeiwe/image/upload/v1687977109/open-space-2_uu2vhu.jpg")
-openspace = Venue.new(
-  name: "Open Space",
-  location: Faker::Address.full_address,
-  description: "Are you looking for a place to enjoy live music with your friends and family? Look no further than
-   Open Space, the ultimate concert venue for all your entertainment needs! Open Space is a spacious outdoor arena that
-    can host any genre of music, from rock to pop, from jazz to classical. You can experience the thrill of seeing your
-    favorite artists perform live, while enjoying the fresh air and nice view",
-  price: 2000
-)
-openspace.photos.attach(io: open1, filename: "opens-1", content_type: "image/png")
-openspace.photos.attach(io: open2, filename: "opens-2", content_type: "image/png")
-openspace.user = user2
-openspace.save
-
-loungephoto = URI.open("https://res.cloudinary.com/dulcgeiwe/image/upload/v1687977109/Lounge-1_hby0di.webp")
-lounge = Venue.new(
-  name: "Lounge Bar",
-  location: Faker::Address.full_address,
-  description: "Looking for a place to relax and have fun? Come to the Lounge Bar, the best venue in town! Enjoy our
-   delicious drinks, cozy atmosphere, and live music every night. Whether you want to chill with your friends, meet new
-   people, or dance the night away, the Lounge Bar has something for everyone. Don't miss our happy hour specials and
-   weekly events. Visit our website or call us to book your table today. The Lounge Bar, where the party never stops!",
-  price: 1400.50
-)
-lounge.photos.attach(io: loungephoto, filename: "lounge-1", content_type: "image/png")
-lounge.user = user1
-lounge.save
-
-cocktailphoto = URI.open("https://res.cloudinary.com/dulcgeiwe/image/upload/v1687977108/cocktail-party_xk1xft.jpg")
-cocktail = Venue.new(
-  name: "Cocktail bar",
-  location: Faker::Address.full_address,
-  description: "Are you looking for a fun and exciting way to spend your evenings? Do you love trying new and delicious
-  cocktails made by expert mixologists? Then you should join the Cocktail Club, the hottest venue in town! The Cocktail
-  Club is more than just a bar, it's a community of cocktail enthusiasts who enjoy socializing, learning, and tasting.
-  Whether you're a beginner or a connoisseur, you'll find something to suit your palate and mood. Plus, you'll get
-   access to exclusive events, discounts, and perks as a member.",
-  price: 1200.20
-)
-cocktail.photos.attach(io: cocktailphoto, filename: "cocktailphoto", content_type: "image/png")
-cocktail.user = user3
-cocktail.save
-
 rexclub1 = URI.open("https://res.cloudinary.com/dulcgeiwe/image/upload/v1689025883/rexclub_1_oveh4a.jpg")
 rexclub2 = URI.open("https://res.cloudinary.com/dulcgeiwe/image/upload/v1689025904/rexclub_2_uftr3z.jpg")
 rexclub = Venue.new(
@@ -168,6 +105,76 @@ social_club.photos.attach(io: social_club1, filename: "social-club-1", content_t
 social_club.photos.attach(io: social_club2, filename: "social-club-2", content_type: "image/png")
 social_club.user = user1
 social_club.save
+
+
+fabric1 = URI.open("https://res.cloudinary.com/dulcgeiwe/image/upload/v1689105492/fabric_1_vddbr1.webp")
+fabric2 = URI.open("https://res.cloudinary.com/dulcgeiwe/image/upload/v1689105503/fabric_2_dcm4rh.jpg")
+fabric = Venue.new(
+  name: "Fabric",
+  location: "77A Charterhouse St, Clerkenwell, London EC1M 6HJ, UK",
+  description: "Fabric is one of London's premier venues and promoters that features weekly line-ups of some of the world's finest talent. The Fabric crew is generally on the ball in terms of running and providing quality events week after week. The FabricLIVE events on Friday feature breaks, drum and bass, while Saturday hosts the finest in European house and techno programmed by resident DJ Craig Richards.",
+  price: 200
+)
+fabric.photos.attach(io: fabric1, filename: "fabric-1", content_type: "image/jpeg")
+fabric.photos.attach(io: fabric2, filename: "fabric-2", content_type: "image/jpeg")
+fabric.user = user1
+fabric.save
+
+
+ministry1 = URI.open("https://res.cloudinary.com/dulcgeiwe/image/upload/v1689105456/ministry_of_sound_1_wcyqih.jpg")
+ministry2 = URI.open("https://res.cloudinary.com/dulcgeiwe/image/upload/v1689105462/ministry_of_sound_2_xsnn7d.png")
+ministry = Venue.new(
+  name: "Ministry of Sound",
+  location: "103 Gaunt St, London SE1 6DP, UK",
+  description: "Ministry of Sound first opened its doors in September 1991 and became the first club dedicated to house music. It quickly gained a reputation as a groundbreaking venue and played a significant role in spreading dance music's popularity around the world. The club is known for its exceptional sound quality, with its main room, The Box, featuring an award-winning sound system.",
+  price: 300
+)
+ministry.photos.attach(io: ministry1, filename: "ministry-of-sound-1", content_type: "image/jpeg")
+ministry.photos.attach(io: ministry2, filename: "ministry-of-sound-2", content_type: "image/jpeg")
+ministry.user = user1
+ministry.save
+
+# Printworks London
+printworks1 = URI.open("https://res.cloudinary.com/dulcgeiwe/image/upload/v1689105412/printworks_1_lyb78l.avif")
+printworks2 = URI.open("https://res.cloudinary.com/dulcgeiwe/image/upload/v1689105419/printworks_2_q1z5a3.jpg")
+printworks = Venue.new(
+  name: "Printworks London",
+  location: "Surrey Quays Rd, London SE16 7PJ, UK",
+  description: "Printworks London is a 5000-capacity licensed venue located within a former printing facility for the Metro and Evening Standard newspapers. It offers a unique space in London's Docklands and hosts a variety of events, combining industrial aesthetics with a vibrant atmosphere.",
+  price: 400
+)
+printworks.photos.attach(io: printworks1, filename: "printworks-london-1", content_type: "image/jpeg")
+printworks.photos.attach(io: printworks2, filename: "printworks-london-2", content_type: "image/jpeg")
+printworks.user = user1
+printworks.save
+
+
+egg1 = URI.open("https://res.cloudinary.com/dulcgeiwe/image/upload/v1689105239/egglondon_1_lg08gf.jpg")
+egg2 = URI.open("https://res.cloudinary.com/dulcgeiwe/image/upload/v1689105245/egg_london_2_qcgfly.jpg")
+egg = Venue.new(
+  name: "Egg London",
+  location: "200 York Way, Kings Cross, London N7 9AX, UK",
+  description: "Egg London is a unique venue located in a Victorian warehouse in Kings Cross. It offers a variety of indoor and outdoor spaces and operates with a 24-hour license, providing a vibrant and dynamic experience for clubbers.",
+  price: 200
+)
+egg.photos.attach(io: egg1, filename: "egg-london-1", content_type: "image/jpeg")
+egg.photos.attach(io: egg2, filename: "egg-london-2", content_type: "image/jpeg")
+egg.user = user1
+egg.save
+
+
+xoyo1 = URI.open("https://res.cloudinary.com/dulcgeiwe/image/upload/v1689105131/xoyo_1_mwsmmy.avif")
+xoyo2 = URI.open("https://res.cloudinary.com/dulcgeiwe/image/upload/v1689105135/xoyo_2_cp1pvf.jpg")
+xoyo = Venue.new(
+  name: "XOYO",
+  location: "32-37 Cowper St, Shoreditch, London EC2A 4AP, UK",
+  description: "XOYO is one of London's most well-respected clubs, known for its consistent and credible music policy. It hosts renowned DJs and artists from around the world, featuring a diverse range of genres. Located in the heart of Shoreditch, XOYO offers a multi-level venue with an expertly-tuned soundsystem and a quarterly residency series.",
+  price: 200
+)
+xoyo.photos.attach(io: xoyo1, filename: "xoyo-1", content_type: "image/jpeg")
+xoyo.photos.attach(io: xoyo2, filename: "xoyo-2", content_type: "image/jpeg")
+xoyo.user = user1
+xoyo.save
 
 
 puts "All done!"
